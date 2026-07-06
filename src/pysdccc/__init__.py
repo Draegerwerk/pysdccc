d5007bd95de266a963d396d6bdb231740ff0972c
Leon Budnick <leon.budnick@draeger.com>
2026-07-06 11:45:40 +0200

    add renovate config

--- src/pysdccc/__init__.py
+++ src/pysdccc/__init__.py
@@ -1,11 +1,18 @@
 """Python wrapper to the SDCcc tool for testing SDC devices."""
 
 from pysdccc._common import DEFAULT_STORAGE_DIRECTORY, check_requirements
-from pysdccc._download import download, download_sync, extract_zip_file, install, is_downloaded, is_downloaded_sync
+from pysdccc._download import (
+    download,
+    download_sync,  # ty:ignore[deprecated]
+    extract_zip_file,
+    install,
+    is_downloaded,
+    is_downloaded_sync,  # ty:ignore[deprecated]
+)
 from pysdccc._result_parser import TestCase, TestSuite
 from pysdccc._runner import SdcccRunner
 from pysdccc._runner_sync import (
-    SdcccRunnerSync,
+    SdcccRunnerSync,  # ty:ignore[deprecated]
 )
 
 __all__ = [
