e372c74f9d76044eeafbaf5ebe3f33f5741cb0a9
Budnick, Leon <leon.budnick@draeger.com>
2026-07-30 15:49:03 +0200

    lint

--- src/pysdccc/_runner/__init__.py
+++ src/pysdccc/_runner/__init__.py
@@ -1,5 +1,5 @@
 from pysdccc._runner.runner_async import SdcccRunner
-from pysdccc._runner.runner_sync import SdcccRunnerSync
+from pysdccc._runner.runner_sync import SdcccRunnerSync  # ty:ignore[deprecated]
 
 __all__ = [
     'SdcccRunner',
